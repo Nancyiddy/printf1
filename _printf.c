02305de3b5768451db69da61bb5e0dc653e93b95
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2022-10-17 15:56:07 +0100

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,9 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _printf - formatted output conversion and print data.
  * @format: input string.
- *
  * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
